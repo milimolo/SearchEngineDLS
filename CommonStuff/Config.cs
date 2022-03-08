e7051447e13e1d217215bc2a532cbd7e18c8009d
milimolo <bondemand@hotmail.dk>
2022-03-08 10:10:01 +0100

    initial commit

---
+++ CommonStuff/Config.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CommonStuff
+{
+    public class Config
+    {
+        public static String FOLDER = @"/Users/bonde/data";
+
+        public static String DATABASE = @"/Users/bonde/database/searchDB.db";
+    }
+}
---
+++ ConsoleSearch/Services/ISearchService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleSearch.Services
+{
+    public interface ISearchService
+    {
+        SearchResult Search(String[] query, int maxAmount);
+    }
+}
